a09d2dc69084ea7ed49bb663c4d16607a6e943fe
Ulugbek <ulugbek4work@gmail.com>
2023-04-10 06:36:03 +0500

    Validate token function implemented

--- utils/enums/error-messages.enum.ts
+++ utils/enums/error-messages.enum.ts
@@ -1,5 +1,6 @@
 export enum ErrorMessages {
   ValidationError = "VALIDATION_ERROR",
   InvalidCredentials = "INVALID_CREDENTIALS",
-  UserNotFound = 'USER_NOT_FOUND'
+  UserNotFound = "USER_NOT_FOUND",
+  TokentIsNotActive = "TOKEN_IS_NOT_ACTIVE",
 }
